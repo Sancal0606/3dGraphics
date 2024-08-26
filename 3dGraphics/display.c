3209f7acd34a2fbabd4aa79cd9e87c52ec1185fe
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-08-26 15:50:36 +0200

    Refactor code, headers

---
+++ 3dGraphics/display.c
@@ -0,0 +1,100 @@
+#include "display.h"
+
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+uint32_t* color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
+
+bool initialize_window(void) {
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL \n");
+		return false;
+	}
+
+	//Use SDL to query what is the fullscreen max width and height
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
+	//Create Window
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		window_width,
+		window_height,
+		SDL_WINDOW_BORDERLESS
+	);
+
+	if (!window) {
+		fprintf(stderr, "Error creating SDL windown \n");
+		return false;
+	}
+
+	//Create SDL renderer
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer \n");
+		return false;
+	}
+
+	//SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
+	return true;
+}
+
+
+void draw_grid(void) {
+	for (int y = 0; y < window_height; y += 10)
+	{
+		for (int x = 0; x < window_width; x += 10)
+		{
+			color_buffer[(window_width * y) + x] = 0xFFFFFFFF;
+		}
+	}
+}
+
+void draw_rectangle(int posX, int posY, int rectangle_width, int rectangle_height, uint32_t color) {
+	for (int y = posY; y < rectangle_height + posY; y++)
+	{
+		for (int x = posX; x < rectangle_width + posX; x++)
+		{
+			color_buffer[(window_width * y) + x] = color;
+		}
+	}
+}
+
+void clear_color_buffer(uint32_t color) {
+	for (int y = 0; y < window_height; y++)
+	{
+		for (int x = 0; x < window_width; x++)
+		{
+			color_buffer[(window_width * y) + x] = color;
+		}
+	}
+}
+
+void render_color_buffer() {
+	SDL_UpdateTexture(
+		color_buffer_texture,
+		NULL,
+		color_buffer,
+		(int)(sizeof(uint32_t) * window_width)
+	);
+	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+
+void destroy_window(void) {
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
---
+++ 3dGraphics/display.h
@@ -0,0 +1,25 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <SDL.h>
+#include<stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+extern SDL_Window* window;
+extern SDL_Renderer* renderer;
+extern SDL_Texture* color_buffer_texture;
+extern uint32_t* color_buffer;
+extern int window_width;
+extern int window_height;
+
+
+bool initialize_window(void);
+void draw_grid(void);
+void draw_rectangle(int posX, int posY, int rectangle_width, int rectangle_height, uint32_t color);
+void clear_color_buffer(uint32_t color);
+void render_color_buffer();
+void destroy_window(void);
+
+
+#endif // !DISPLAY_H
