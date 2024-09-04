26c100ee886045ff6aa6e5663e43d518b1cdef11
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-09-04 16:59:30 +0200

    Fix division with 0

--- 3dGraphics/triangle.c
+++ 3dGraphics/triangle.c
@@ -52,11 +52,21 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
 		int_swap(&y0, &y1);
 		int_swap(&x0, &x1);
 	}
-	//Calculate the new vertex (Mx,My)
-	int My = y1;
-	int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
-	//Draw flat-bottom triangle
-	fill_flat_bottom_triangle(x0,y0,x1,y1,Mx,My,color);
-	//Draw flat-top trinagle
-	fill_flat_top_triangle(x1,y1,Mx,My,x2,y2,color);
+
+	if (y1 == y2) {
+		fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+	}
+	else if (y0 == y1) {
+		fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+	}
+	else {
+
+		//Calculate the new vertex (Mx,My)
+		int My = y1;
+		int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+		//Draw flat-bottom triangle
+		fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+		//Draw flat-top trinagle
+		fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+	}
 }
