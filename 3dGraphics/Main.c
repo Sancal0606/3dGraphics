f8e439581a791a1f7bb5b11a6c9a23a15d202903
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-09-06 13:49:36 +0200

    World matrix

--- 3dGraphics/Main.c
+++ 3dGraphics/Main.c
@@ -145,6 +145,14 @@ void update(void) {
 	mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
 	mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
 
+	//Create a world matrix combining scale, rotation and translation matrices
+	mat4_t world_matrix = mat4_identity();
+	world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+	world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+	world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+	world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+	world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
+
 	int num_faces = array_length(mesh.faces);
 	//Loop all triangle faces of our mesh
 	for (int i = 0; i < num_faces; i++)
@@ -163,15 +171,12 @@ void update(void) {
 		for (int j = 0; j < 3; j++)
 		{
 			vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
+			 
+			
 
-			//Use a matrix to scale our original vertex
-			transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-			//Use matrices to rotate vertex
-			transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
-			transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
-			transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
-			//Use a matrix to translate vertex
-			transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
+			//Multiply the world matrix by the original vector
+			transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
+			
 			
 			//Translate the vertex away from the camera
 			transformed_vertices[j] = transformed_vertex;
