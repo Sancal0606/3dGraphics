67a09615bc7ba9ac92afec863f3e91cd4da030a0
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-09-06 09:00:16 +0200

    Rotation matrices

--- 3dGraphics/matrix.h
+++ 3dGraphics/matrix.h
@@ -14,5 +14,9 @@ vec4_t mat4_mul_vec4(mat4_t m, vec4_t v);
 
 mat4_t mat4_make_translation(float tx, float ty, float tz);
 
+mat4_t mat4_make_rotation_z(float angle);
+mat4_t mat4_make_rotation_x(float angle);
+mat4_t mat4_make_rotation_y(float angle);
+
 
 #endif // !MATRIX_H
