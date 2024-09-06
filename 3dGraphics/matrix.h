2237f9e8bd06509ab45a912ec53e51dd08cd1d81
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-09-06 08:32:35 +0200

    Translation matrix

--- 3dGraphics/matrix.h
+++ 3dGraphics/matrix.h
@@ -12,5 +12,7 @@ mat4_t mat4_make_scale(float sx, float sy, float sz);
 
 vec4_t mat4_mul_vec4(mat4_t m, vec4_t v);
 
+mat4_t mat4_make_translation(float tx, float ty, float tz);
+
 
 #endif // !MATRIX_H
