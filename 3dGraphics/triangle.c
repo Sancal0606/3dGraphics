0e69b9e4e96467ff5f2f6d0d3aae50c153e8652a
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-09-09 18:34:27 +0200

    Texture Upper triangle

--- 3dGraphics/triangle.c
+++ 3dGraphics/triangle.c
@@ -1,10 +1,6 @@
 #include "triangle.h"
 
-void int_swap(int* a, int* b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
+
 
 void fill_flat_bottom_triangle(int x0, int y0, int x1,int y1, int x2,int y2, uint32_t color) {
 	float inv_slope = (float)(x1 - x0) / (y1 - y0);
@@ -77,7 +73,49 @@ void draw_texture_triangle(
 	int x2, int y2, float u2, float v2,
 	uint32_t* texture) 
 {
-	return;
+	//Sort tge vertices by y-cordinate ascending (y0<y1<y2)
+	if (y0 > y1) {
+		int_swap(&y0, &y1);
+		int_swap(&x0, &x1);
+		int_swap(&u0, &u1);
+		int_swap(&v0, &v1);
+	}
+	if (y1 > y2) {
+		int_swap(&y1, &y2);
+		int_swap(&x1, &x2);
+		float_swap(&u1, &u2);
+		float_swap(&v1, &v2);
+	}
+	if (y0 > y1) {
+		int_swap(&y0, &y1);
+		int_swap(&x0, &x1);
+		int_swap(&u0, &u1);
+		int_swap(&v0, &v1);
+	}
+
+	//Upper triangle (flat-bottom)
+	float inv_slope_1 = 0;
+	float inv_slope_2 = 0;
+
+	if (y1 != y0) inv_slope_1 = (float)(x1 - x0) / abs(y1 - y0);
+	if (y2 != y0) inv_slope_2 = (float)(x2 - x0) / abs(y2 - y0);
+
+	if (y1 - y0 != 0) {
+		for (int y = y0; y <= y1; y++)
+		{
+			int x_start = x1 + (y - y1) * inv_slope_1;
+			int x_end = x0 + (y - y0) * inv_slope_2;
+
+			if (x_start > x_end)
+				int_swap(&x_start, &x_end);
+
+			for (int x = x_start; x <= x_end; x++)
+			{
+				draw_pixel(x, y, 0xFFFF00FF);
+			}
+		}
+	}
+
 }
 
 
--- 3dGraphics/triangle.h
+++ 3dGraphics/triangle.h
@@ -5,6 +5,7 @@
 #include<stdint.h>
 #include "display.h"
 #include "texture.h"
+#include "swap.h"
 
 typedef struct {
 	int a;
@@ -24,7 +25,7 @@ typedef struct {
 } triangle_t;
 
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
-void int_swap(int* a, int* b);
+
 void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 void fill_flat_top_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 void draw_texture_triangle(
